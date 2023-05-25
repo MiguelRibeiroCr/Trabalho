922324d3f844c2c0a9ad102879cd189a28bae507
MiguelRibeiroCr <miguelribe715@gmail.com>
2023-05-24 21:24:37 -0300

    Trabalho Camera

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
 
 export default function App() {
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHaspermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHaspermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text> Acesso negado! </Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <CameraType style={{ flex: 1 }} type={type}>
+          <View style={{flex: 1, backgroundColor: 'transparent', flexDirection: 'row'}}>
+            <TouchableOpacity 
+            style={{
+              position: 'absolute',
+              bottom: 20,
+              left: 20,
+            }}
+            onPress={() =>{
+              setType(
+                type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+              );
+            }}
+            >
+
+            </TouchableOpacity>
+          </View>
+      </CameraType>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
