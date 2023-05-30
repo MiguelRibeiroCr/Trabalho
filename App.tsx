d8ac51647ebdb75bda1ebf1eba4b6a6542966824
MiguelRibeiroCr <miguelribe715@gmail.com>
2023-05-29 21:35:15 -0300

    Camera

--- App.tsx
+++ App.tsx
@@ -1,55 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
+import React, { useEffect, useState } from 'react';
+import { Button, View, StyleSheet } from 'react-native';
 
-export default function App() {
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  const [hasPermission, setHaspermission] = useState(null);
+export default function App (){
+    const [image, setImage] = useState(null);
+    const [camera, setCamera] = useState(null);
+    const [permission, setPermission] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHaspermission(status === 'granted');
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+          const  cameraStatus  = await Camera.requestCameraPermissionsAsync();
+          setPermission(cameraStatus.status === 'granted');
+        })();
+      }, []);
 
-  if (hasPermission === null) {
-    return <View />;
-  }
+      async function takePicture(){
+        if (camera){
+        
+        const photo = await camera.takePictureAsync();
+        console.log(photo.uri);
+      }
+    }  
 
-  if (hasPermission === false) {
-    return <Text> Acesso negado! </Text>;
-  }
+    return(
+        <View style={styles.container}>
+            <Camera
+            ref={(ref)=> setCamera(ref)}
+            style={styles.Camera}
+            type={CameraType.back}
+            ratio={'1:1'}
+            />
+            <Button title ="" onPress={()=>{takePicture()}}/>
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <CameraType style={{ flex: 1 }} type={type}>
-          <View style={{flex: 1, backgroundColor: 'transparent', flexDirection: 'row'}}>
-            <TouchableOpacity 
-            style={{
-              position: 'absolute',
-              bottom: 20,
-              left: 20,
-            }}
-            onPress={() =>{
-              setType(
-                type === Camera.Constants.Type.back
-                ? Camera.Constants.Type.front
-                : Camera.Constants.Type.back
-              );
-            }}
-            >
-
-            </TouchableOpacity>
-          </View>
-      </CameraType>
-    </SafeAreaView>
-  );
+        </View>
+    );
+    
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
+    container:{
+        flex:1
+    },
+
+    Camera:{
+        aspectRatio:1,
+        flex: 1,
+
+    }
+    
 });
